69891b7ffdbf7c2351cd0cc909da1641b0b5cf12
domoi <103207883+domoi@users.noreply.github.com>
2023-04-25 18:36:46 +0700

    Create templates change_profile

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='users', verbose_name='Фото',null=True, blank=True)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Фото', null=True, blank=True)
